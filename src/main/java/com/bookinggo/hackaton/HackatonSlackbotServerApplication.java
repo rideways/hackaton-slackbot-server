bdc7d2ae11d3a4aa7547203b8f0b0f7432cf9ba9
Paweł Szymański <pawelszymanski@rentalcars.com>
2018-04-23 08:53:11 +0100

    Spring project initialization

---
+++ src/main/java/com/bookinggo/hackaton/HackatonSlackbotServerApplication.java
@@ -0,0 +1,12 @@
+package com.bookinggo.hackaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonSlackbotServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackatonSlackbotServerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/bookinggo/hackaton/HackatonSlackbotServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bookinggo.hackaton;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HackatonSlackbotServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
