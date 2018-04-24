762927f3f8144b1ecd70674a42ed093a57f6354a
Paweł Szymański <pawelszymanski@rentalcars.com>
2018-04-24 16:07:17 +0100

    Make forker a bean

--- src/main/java/com/bookinggo/hackaton/HackatonSlackbotServerApplication.java
+++ src/main/java/com/bookinggo/hackaton/HackatonSlackbotServerApplication.java
@@ -5,18 +5,27 @@ import com.bookinggo.hackaton.domain.process.ProcessSplitter.ProcessForker;
 import com.bookinggo.hackaton.domain.worker.ScriptExecutorWorkerApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static java.util.Objects.*;
 
 @SpringBootApplication
 public class HackatonSlackbotServerApplication {
+
+    private static ProcessForker FORKER;
 
     public static void main(String[] args) {
         ProcessSplitter.splitProcess(forker -> springAppMain(args, forker),
                                      () -> ScriptExecutorWorkerApplication.main(args));
     }
 
-    public static void springAppMain(String[] args, ProcessForker forker) {
+    private static void springAppMain(String[] args, ProcessForker forker) {
+        FORKER = forker; // TODO: workaround for bean creation, could be improved, possibly.
         SpringApplication.run(HackatonSlackbotServerApplication.class, args);
+    }
 
-        // TODO: put forker into a bean
+    @Bean
+    ProcessForker forker() {
+        return requireNonNull(FORKER);
     }
 }
