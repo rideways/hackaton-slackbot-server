01891b581cc1b9421d3393fd1a26813834d90316
Ricardo Passarella <RicardoPassarella@rentalcars.com>
2018-04-25 21:58:51 +0100

    Changed groovy script2

--- src/example/groovy/ExampleScript2.groovy
+++ src/example/groovy/ExampleScript2.groovy
@@ -1,3 +1,3 @@
-{ String sender, String channel, String message ->
-    "Hello " + sender + " and welcome on " + channel + ". Message was: " + message
+{ String message ->
+    "Hello. Message was: " + message
 }
